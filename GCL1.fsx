54fbd1c670752506dc9563ac89072658b557fe75
Rasmus <rasmus.mm@gmail.com>
2020-03-17 12:09:20 +0100

    Part 2. Changes in TypeAST and GCL1.fsx

--- GCL1.fsx
+++ GCL1.fsx
@@ -16,7 +16,7 @@ open GCL1Lexer
 
 open System.IO
 
-
+(*
 // The parser 
 let parse input =
 
@@ -25,6 +25,16 @@ let parse input =
     let res= GCL1Parser.start GCL1Lexer.tokenize lexbuf
 
     res
+*)
+let parse input =
+    // translate string into a buffer of characters
+    // translate the buffer into a stream of tokens and parse them
+    try
+        let lexbuf = LexBuffer<char>.FromString input
+        Some(GCL1Parser.start GCL1Lexer.tokenize lexbuf)
+        
+    with |Failure msg->
+        None
 
 ///<summary>
 /// The function GCL implements the parser and checks the syntax 
@@ -38,6 +48,7 @@ let parse input =
 /// By default we use Console.ReadLine() 
 ///</remarks>
 /// 
+(*
 let GCL =  
 
     let input = Console.ReadLine() 
@@ -48,3 +59,17 @@ let GCL =
        printf "Program accepted!\n"
     with err -> 
         printf "Program rejected!\n"
+*)
+
+let GCL =  
+    let file = File.ReadAllText("input.txt")
+    printf "input:\n\n%s\n" file
+    let program : Command option = parse (file)
+    match program with 
+        | None -> printf "ko\n"
+        | Some(p) -> 
+            printf("syntax ok\n")
+            let pg = ProgramGraph p
+            let wr = new System.IO.StreamWriter("graph.gv")
+            pg |> wr.Write
+            wr.Close() 
--- GCL1TypeAST.fs
+++ GCL1TypeAST.fs
@@ -38,3 +38,150 @@ and GuardedCommand =
     | GC of BExpr*Command
     | GCs of GuardedCommand*GuardedCommand
 
+// Definitions of parentheses used for syntax
+let lpar leftpar = if leftpar then "(" else ""
+let rpar rightpar = if rightpar then ")" else ""
+
+// matching for aexpression for graph
+type AExpr with 
+    member this.Eval env = 
+        match this with
+        | Num(float) -> float
+        | Var(string) -> match Map.tryFind string env with
+                            | Some(v) -> v
+                            | None -> 0.0
+        | PlusExpr(e1,e2) -> (e1.Eval env) + (e2.Eval env)
+        | MinusExpr(e1,e2) -> (e1.Eval env) - (e2.Eval env)
+        | TimesExpr(e1,e2) -> (e1.Eval env) * (e2.Eval env) 
+        | DivExpr(e1,e2) -> (e1.Eval env) / (e2.Eval env) 
+        | PowExpr(e1,e2) -> System.Math.Pow((e1.Eval env), (e2.Eval env))
+        | Neg(e1) -> - (e1.Eval env)
+
+    member this.Atomic = 
+        match this with
+        | Num(_) |Var(_) |Neg(_)-> true 
+        | PlusExpr(_) | MinusExpr(_) | TimesExpr(_) | DivExpr(_) | PowExpr(_) -> false
+       
+    member this.ToString =
+        match this with
+        | Num(f) -> string(f)
+        | Var(s) -> s
+        | PlusExpr(e1,e2) -> (lpar (not e1.Atomic)) + e1.ToString + (rpar (not e1.Atomic)) + "+" + (lpar (not e2.Atomic)) +  e2.ToString  + (rpar (not e2.Atomic))
+        | MinusExpr(e1,e2) -> (lpar (not e1.Atomic)) + e1.ToString + (rpar (not e1.Atomic)) + "-" + (lpar (not e2.Atomic)) +  e2.ToString  + (rpar (not e2.Atomic))
+        | TimesExpr(e1,e2) -> (lpar (not e1.Atomic)) + e1.ToString + (rpar (not e1.Atomic)) + "*" + (lpar (not e2.Atomic)) +  e2.ToString  + (rpar (not e2.Atomic))
+        | DivExpr(e1,e2) -> (lpar (not e1.Atomic)) + e1.ToString + (rpar (not e1.Atomic)) + "/" + (lpar (not e2.Atomic)) +  e2.ToString  + (rpar (not e2.Atomic)) 
+        | PowExpr(e1,e2) -> (lpar (not e1.Atomic)) + e1.ToString + (rpar (not e1.Atomic)) + "^" + (lpar (not e2.Atomic)) +  e2.ToString  + (rpar (not e2.Atomic)) 
+        | Neg(e1) -> "-" + (lpar (not e1.Atomic)) + e1.ToString + (rpar (not e1.Atomic))
+
+// matching for bexpression for graph
+type BExpr with 
+    member this.Eval env = 
+        match this with 
+        | Bool(b) -> b
+        | And(b1,b2) -> 
+            let v1 = (b1.Eval env) 
+            let v2 = (b2.Eval env)
+            v1 && v2
+        | Or(b1,b2) -> 
+            let v1 = (b1.Eval env) 
+            let v2 = (b2.Eval env)
+            v1 || v2
+        | Sand(b1,b2) -> (b1.Eval env) && (b2.Eval env)
+        | Sor(b1,b2) -> (b1.Eval env) || (b2.Eval env)
+        | Not(b) -> not (b.Eval env)
+        | Eq(a1,a2) -> (a1.Eval env) = (a2.Eval env)
+        | Neq(a1,a2) -> (a1.Eval env) <> (a2.Eval env)
+        | Gt(a1,a2) -> (a1.Eval env) > (a2.Eval env)
+        | Gte(a1,a2) -> (a1.Eval env) >= (a2.Eval env)
+        | Lt(a1,a2) -> (a1.Eval env) < (a2.Eval env)
+        | Lte(a1,a2) -> (a1.Eval env) <= (a2.Eval env)
+
+    member this.Atomic = 
+        match this with
+        | Bool(_) -> true
+        | Not(_) | Eq(_) | Neq(_) | Lt(_) | Lte(_) | Gt(_) | Gte(_)-> false
+        | Or(_) | Sor(_) | And(_) | Sand(_) ->   false
+         
+    member this.ToString = 
+        match this with
+        | Bool(b) -> string(b)
+        | And(b1,b2) ->  (lpar (not b1.Atomic)) + b1.ToString + (rpar (not b2.Atomic)) + " & " + (lpar (not b2.Atomic)) + b2.ToString + (rpar (not b2.Atomic))
+        | Or(b1,b2) ->   (lpar (not b1.Atomic)) + b1.ToString + (rpar (not b2.Atomic)) + " | " + (lpar (not b2.Atomic)) + b2.ToString + (rpar (not b2.Atomic))
+        | Sand(b1,b2) -> (lpar (not b1.Atomic)) + b1.ToString + (rpar (not b2.Atomic)) + " && " + (lpar (not b2.Atomic)) + b2.ToString + (rpar (not b2.Atomic))
+        | Sor(b1,b2) ->  (lpar (not b1.Atomic)) + b1.ToString + (rpar (not b2.Atomic)) + " || " + (lpar (not b2.Atomic)) + b2.ToString + (rpar (not b2.Atomic))
+        | Not(b) -> "!"+ (lpar (not b.Atomic)) + b.ToString + (rpar (not b.Atomic))
+        | Eq(a1,a2) ->   (lpar (not a1.Atomic)) + a1.ToString + (rpar (not a2.Atomic)) + " = " + (lpar (not a2.Atomic)) + a2.ToString + (rpar (not a2.Atomic))
+        | Neq(a1,a2) ->  (lpar (not a1.Atomic)) + a1.ToString + (rpar (not a2.Atomic)) + " != " + (lpar (not a2.Atomic)) + a2.ToString + (rpar (not a2.Atomic))
+        | Gt(a1,a2) ->   (lpar (not a1.Atomic)) + a1.ToString + (rpar (not a2.Atomic)) + " > " + (lpar (not a2.Atomic)) + a2.ToString + (rpar (not a2.Atomic))
+        | Gte(a1,a2) ->  (lpar (not a1.Atomic)) + a1.ToString + (rpar (not a2.Atomic)) + " >= " + (lpar (not a2.Atomic)) + a2.ToString + (rpar (not a2.Atomic))
+        | Lt(a1,a2) ->   (lpar (not a1.Atomic)) + a1.ToString + (rpar (not a2.Atomic)) + " < " + (lpar (not a2.Atomic)) + a2.ToString + (rpar (not a2.Atomic))
+        | Lte(a1,a2) ->  (lpar (not a1.Atomic)) + a1.ToString + (rpar (not a2.Atomic)) + " <= " + (lpar (not a2.Atomic)) + a2.ToString + (rpar (not a2.Atomic))
+
+// noexecute for GuardedCommands
+let rec noexecute = function 
+    | GC(b,_) -> "!(" + b.ToString + ")"
+    | GCs(gc1,gc2) -> noexecute gc1 + " & " + noexecute gc2
+
+// for printing transitions in .gv file
+let transition first second label = "\tq"+first+" -> "+"q"+second+" [label = \""+label+"\"];\n"
+
+// command part in GCLParser for graph.
+let rec CommandGraph first second n = function
+    | Skip -> transition first second "skip" , n
+    | Ass(name,value) -> transition first second (name + " := " + value.ToString) , n
+    | Commands(c1,c2) -> CommandGraph first (string(n)) (n+1) c1 |> fun (s1,n1) ->  CommandGraph (string(n)) second (n1) c2 |> fun (s2,n2) -> s1+s2, n2
+    | If(gc) -> GuardedCommandGraph first second n gc
+    | Do(gc) -> GuardedCommandGraph first first n gc |> fun (graph, n) -> graph + (transition first second  (noexecute gc) ), n
+and GuardedCommandGraph first second n = function 
+    | GC(b,c) -> let str,n' = CommandGraph (string(n)) second (n+1) c
+                 (transition first (string(n)) b.ToString) + str, n'
+    | GCs(gc1,gc2) -> let s1,n1 = (GuardedCommandGraph first second (n) gc1)
+                      let s2,n2 = (GuardedCommandGraph first second (n1) gc2)
+                      (s1+s2),n2
+
+// header taken from http://www.formalmethods.dk/fm4fun/ (export graph). 
+let header =   "digraph program_graph {rankdir=LR;
+node [shape = circle]; q▷;
+node [shape = doublecircle]; q◀; 
+node [shape = circle]\n"
+
+// footer taken from http://www.formalmethods.dk/fm4fun/ (export graph)
+let footer = "}"
+
+// definition of program which will be read by GCL.fsx. This will construct a program with
+// a header, transitions and footer. The structure is: 
+///////////////////HEADER////////////////////////
+//              digraph program_graph {rankdir=LR;
+//              node [shape = circle]; q▷;
+//              node [shape = doublecircle]; q◀; 
+//              node [shape = circle]
+//////////////////TRANSITIONS////////////////////
+//              q▷ -> qn [label = ""]; etc, where n is the states.
+//////////////////FOOTER////////////////////
+//              }
+
+let ProgramGraph program = let s, _ = CommandGraph "▷" "◀" 1 program
+                           header + s + footer
+
+// Definition of GuardedCommand used for graph. 
+type GuardedCommand with
+    member this.TryFind env = 
+        match this with
+        | GC(b,c) -> if b.Eval env then Some(c) else None
+        | GCs(gc1,gc2) -> match gc1.TryFind env with
+                          | None -> gc2.TryFind env
+                          | s -> s
+
+// Lastly the command used for graph. 
+type Command with 
+    member this.Exec env = 
+        match this with
+        | Ass(s, aexpr) -> Map.add s (aexpr.Eval env) env
+        | Skip -> env
+        | Commands(c1,c2) -> c2.Exec (c1.Exec env) 
+        | If(gc) -> match gc.TryFind env with
+                    | Some(c) -> c.Exec env
+                    | None -> failwith("Unable to construct path")
+        | Do(gc) -> match gc.TryFind env with
+                    | Some(c) -> this.Exec (c.Exec env)
+                    | None -> env
